eb5e5e67efc417b59e1c0adbb6df1d5c7d4801e3
Abdullah Afzal <abdullah941685@gmail.com>
2024-06-04 19:58:59 +0500

    first comment

---
+++ JS/script.js
@@ -0,0 +1,55 @@
+let users = [];
+let todos = [];
+
+function generateUID() {
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+const handlesubmit = () => {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    if (users.find(user => user.email === email)) {
+        alert('User already exists');
+    } else {
+        const newUser = { email:email, password:password, uid: generateUID(), status: 'active', createdAt: new Date() };
+        users.push(newUser);
+        alert('Registration successful!');
+        window.location.href = 'login.html';
+    }
+}
+
+const handlelogin = () => {
+    event.preventDefault();
+    const email = document.getElementById('loginEmail').value;
+    const password = document.getElementById('loginPassword').value;
+    const user = users.find(user => user.email == email && user.password == password);
+    if (user) {
+        sessionStorage.setItem('loggedInUser', JSON.stringify(user));
+        window.location.href = 'todos.html';
+    } else {
+        alert('Invalid email or password');
+    }
+}
+
+const todo = () => {
+
+
+    event.preventDefault();
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+    const date = document.getElementById('date').value;
+    const loggedInUser = JSON.parse(sessionStorage.getItem('loggedInUser'));
+    const newTodo = {
+        title,
+        description,
+        date,
+        id: generateUID(),
+        status: 'pending',
+        createdAt: new Date(),
+        user_id: loggedInUser.uid
+    };
+    todos.push(newTodo);
+    displayTodos();
+}
+
