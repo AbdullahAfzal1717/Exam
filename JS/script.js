38f8082af29fd05473882dccd92adfdf49e657c9
AbdullahAfzal1717 <161310235+AbdullahAfzal1717@users.noreply.github.com>
2024-06-04 23:33:06 +0500

    Second comment

--- JS/script.js
+++ JS/script.js
@@ -52,4 +52,23 @@ const todo = () => {
     todos.push(newTodo);
     displayTodos();
 }
+function displayTodos() {
+    const loggedInUser = JSON.parse(sessionStorage.getItem('loggedInUser'));
+    const userTodos = todos.filter(todo => todo.user_id === loggedInUser.uid);
+    const todoList = document.getElementById('todoList');
+    todoList.innerHTML = '';
+    userTodos.forEach(todo => {
+        const li = document.createElement('li');
+        li.className = 'list-group-item';
+        li.innerHTML = `<strong>${todo.title}</strong> - ${todo.description} - ${todo.date}`;
+        todoList.appendChild(li);
+    });
+}
 
+window.onload = function() {
+    const loggedInUser = JSON.parse(sessionStorage.getItem('loggedInUser'));
+    if (loggedInUser) {
+        document.getElementById('userEmail').innerText = loggedInUser.email;
+        displayTodos();
+    }
+};
